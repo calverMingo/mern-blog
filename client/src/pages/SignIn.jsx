314e9a9544dce53977175b2cbb56502a3677fd1e
Vivek <vivek.jain.cs@outlook.com>
2024-02-07 03:59:04 +0530

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -21,7 +21,6 @@ export default function Signin() {
       return { ...formData, [e.target.id]: e.target.value.trim() };
     });
   };
-  console.log(formData);
 
   const handleSubmit = async (e) => {
     e.preventDefault();

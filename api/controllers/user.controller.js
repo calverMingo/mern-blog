a06eca87f8ba9dd581e691ffef85e50127df552e
Vivek <vivek.jain.cs@outlook.com>
2024-02-12 04:17:39 +0530

    add update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,63 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ msg: "API is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    next(errorHandler(403, "you are not allowed to update the user"));
+  }
+
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(
+        errorHandler(400, "password must be atleast of 6 characters")
+      );
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "username must be between 7 and 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "username must not contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(
+        errorHandler(400, "username must contain only lowercase characters")
+      );
+    }
+
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "username can contain numbers and letters")
+      );
+    }
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
